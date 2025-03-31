d6508350652b8496109551f423a046551eb6f63f
bao1311 <105867837+bao1311@users.noreply.github.com>
2025-03-31 13:44:02 -0700

    Initial Commit

---
+++ EventFinder/EventFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventFinderApp.swift
+//  EventFinder
+//
+//  Created by Gia Bao Phi on 3/31/25.
+//
+
+import SwiftUI
+
+@main
+struct EventFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
